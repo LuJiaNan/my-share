3fd5092cd26f55d0cee48ea2a8fc21663267030b
lujianan <>
2020-03-16 15:21:36 +0800

    添加cypress report

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,6 +17,7 @@ const { initPlugin } = require('cypress-plugin-snapshots/plugin');
 
 module.exports = (on, config) => {
   initPlugin(on, config);
+  on('task', require('@cypress/code-coverage/task'))
   return config;
 };
 // module.exports = (on, config) => {
